9f86461b8ea9bc04ee151b343a82ff771368422a
Erik172 <alejod172@gmail.com>
2020-11-26 21:59:55 -0500

    firstVersion

---
+++ app.py
@@ -0,0 +1,76 @@
+# Archivo Principal
+from flask import Flask, request, render_template
+from flask_restful import Resource, Api, abort
+import models
+import datetime
+import time
+
+app = Flask(__name__)
+api = Api(app)
+
+### CURSOS ###
+
+@app.route('/api')
+def home():
+    return 'Hola'
+
+class Courses(Resource):
+    # Funcion para mostrar todos los cursos
+    def get(self):
+        courses = models.showCourses()
+        return courses
+
+    # Funcion para agregar cursos
+    def post(self):
+        data = request.get_json(force=True)
+        models.addCourse(data)
+
+        return data, 201
+
+    # Funcion para eliminar !TODOS LOS CURSOS!
+    def delete(self):
+        models.deleteAllCourses()
+
+        return 201
+
+class CoursesId(Resource):
+    # Funcion para buscar un curso por su _id
+    def get(self, id: int):
+        course = models.searchCourseId(id)
+        return course
+
+    # Funcion para editar un curso por su _id
+    def put(self, id: int):
+        data = request.get_json(force=True)
+        models.modifiedCourseId(id, data)
+
+        return data, 201
+
+    # Funcion para Eliminar un curso por su _id
+    def delete(self, id: int):
+        models.deleteCourseId(id)
+
+        return 201
+
+### POSTS ###
+
+class Posts(Resource):
+    def get(self):
+        posts = models.showPosts()
+        return posts
+
+class PostsId(Resource):
+    def get(self, id: int):
+        post = models.searchPostId(id)
+        return post
+
+# Ruta para todos los cursos
+api.add_resource(Courses, '/api/courses')
+
+# Ruta para las funciones dentro de la clase CoursesId
+api.add_resource(CoursesId, '/api/courses/<int:id>')
+
+api.add_resource(Posts, '/api/posts')
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8088)
---
+++ models.py
@@ -0,0 +1,68 @@
+# Archivo para manejar todo lo de la base de datos
+from pymongo import MongoClient
+from bson import json_util
+import json
+import os
+
+# Conectarse a la base de datos MongoDB de manera local
+#client = MongoClient(os.environ["DB_HOST"], int(os.environ["DB_PORT"]))
+
+# Conectarse a la base de datos en producion
+client = MongoClient(os.environ["MONGO_DB"])
+
+db = client.platzi # Base de datos nombrada como platzi
+courses = db.courses # Colecion de cursos
+posts = db.posts # Colecion de posts (Blog de Platzi)
+users = db.users # Colecion de usuarios (De la API)
+
+### Cursos ###
+
+# Funcion para buscar y mostrar todos los cursos
+def showCourses():
+    data = courses.find()
+    result = []
+    for r in data:
+        result.append(r)
+
+    # Retorna un archivo json con todos los cursos
+    return json.loads(json_util.dumps(result))
+
+# Funcion para agregar un curso
+def addCourse(data: dict):
+    courses.insert_one(data)
+
+# Funcion para !ELIMINAR TODOS LOS CURSOS!
+def deleteAllCourses():
+    courses.delete_many({})
+
+# Funcion para buscar un curso por su _id
+def searchCourseId(id: int):
+    data = courses.find_one({"_id": id})
+    return data
+
+# Funcion para modificar un curso por su _id
+def modifiedCourseId(id: int, data: dict):
+    courses.find_and_modify({"_id": id}, data)
+
+# Funcion para eliminar un curso por su _id
+def deleteCourseId(id: int):
+    courses.find_one_and_delete({"_id": id})
+
+### Posts ###
+
+# Funcion Para agregar Post
+def addPost(data: dict):
+    posts.insert_one(data)
+
+# Funcion para mostrar todos los posts
+def showPosts():
+    data = posts.find()
+    result = []
+    for r in data:
+        result.append(r)
+
+    return json.loads(json_util.dumps(result))
+
+# Funcion para buscar un post por su id
+def searchPostId(id: int):
+    data = posts.find_one({'_id': id})
